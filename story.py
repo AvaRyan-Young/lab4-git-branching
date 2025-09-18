5d3e002565a84a78c9693c0208b5c0e8d82443f9
Ava Ryan-Young <aryanyou@emich.edu>
2025-09-17 22:56:40 -0400

    Added line to hero path

--- story.py
+++ story.py
@@ -11,6 +11,7 @@ def intro():
 def left_path():
     print("You walk left and find a mysterious glowing sword stuck in a stone.")
     print("You take the sword to complete your sacred quest.")
+    print("The townsfolk need you to defeat an evil dragon!")
 
 def right_path():
     print("You walk right and encounter a talking squirrel who challenges you to a duel.")
